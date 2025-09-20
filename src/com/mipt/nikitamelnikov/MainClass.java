17ef0b0bf6dd35c5e40e842515fd869e5359cdee
Nikita Melnikov <mnd040807@gmail.com>
2025-09-20 16:44:38 +0300

    maven project hw2

---
+++ src/com/mipt/nikitamelnikov/MainClass.java
@@ -0,0 +1,14 @@
+package com.mipt.nikitamelnikov;
+
+public class MainClass {
+    private int number;
+    private String letter;
+    protected static double db;
+    public final long number1 = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Iter: " + i);
+        }
+    }
+}
---
+++ src/com/mipt/nikitamelnikov/Students.java
@@ -0,0 +1,5 @@
+package com.mipt.nikitamelnikov;
+
+public interface Students {
+    Object study(Object input);
+}
